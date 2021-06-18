1a93cba6cf664d26b2360c93fe29afe784522d5f
divya <divya@DESKTOP-05C4R5A>
2021-06-18 12:24:13 +0530

    180040087

---
+++ project2/src/VirtualKeyApplication.java
@@ -0,0 +1,11 @@
+import screens.Welcome;
+
+public class VirtualKeyApplication {
+	 public static void main(String[] args) {
+	    	
+	    	Welcome welcome = new Welcome();
+	    	welcome.introWS();
+	    	welcome.GetUserInput();
+
+	    }
+}
